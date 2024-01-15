7d8ab8ba957609f9440dee1a9aab61777369a3e6
RamiyanGangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-01-15 12:10:21 -0500

    Add files via upload

---
+++ EnterpriseAssignments/inft2201.gangatharanr/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * This is the main class where everything will be executed.
+ * @author Ramiyan Gangatharan
+ * @version 1.0 (2024/01/14)
+ * @since 1.0
+ */
+
+public class Main
+{
+    /**
+     * This method will execute all the code I create in this course.
+     * @param args
+     * @return string
+     * //@exception something
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
