12522c37648fa662be8295d4a8c03d97158e716d
Ramiyan Gangatharan <135138789+RamiyanGangatharan@users.noreply.github.com>
2024-01-23 11:01:57 -0500

    Add files via upload

--- EnterpriseAssignments/inft2201.gangatharanr/src/CollegeInterface.java
+++ EnterpriseAssignments/inft2201.gangatharanr/src/CollegeInterface.java
@@ -1,5 +1,8 @@
 /**
- * This is the interface for this course.
+ * This interface defines constants used throughout the course project. It contains predefined constants for the college
+ * name and its phone number. These constants can be used in implementing classes to maintain consistency and
+ * standardization of college-related information.
+ *
  * @author Ramiyan Gangatharan
  * @version 1.2 (January 22, 2024)
  * @since 1.0 (January 14, 2024)
@@ -8,9 +11,6 @@ public interface CollegeInterface
 {
     String COLLEGE_NAME = "Durham college";
     String PHONE_NUMBER = "(905) 721 2000";
-    String formattedString =
-            "" +
-                    "College: " + COLLEGE_NAME + "\n" +
-                    "Phone Number: " + PHONE_NUMBER;
+
 
 }
--- EnterpriseAssignments/inft2201.gangatharanr/src/Main.java
+++ EnterpriseAssignments/inft2201.gangatharanr/src/Main.java
@@ -1,5 +1,7 @@
 /**
- * This is the main class where everything will be executed.
+ * This is the main class where everything will be executed. It creates an instance
+ * of the User class and calls its dump method to display the user's information.
+ *
  * @author Ramiyan Gangatharan
  * @version 1.2 (January 22, 2024)
  * @since 1.0 (January 14, 2024)
@@ -8,12 +10,17 @@
 public class Main
 {
     /**
-     * This method will execute all the code I create in this course.
-     * @param args
+     * The entry point of the application.
+     *
+     * This is the main method which serves as the starting point of the program.
+     * It creates a new User object and invokes its dump method to print the user's
+     * information to the standard output.
+     *
+     * @param args command-line arguments passed to the program. Not used in this application.
      */
     public static void main(String[] args)
     {
-        System.out.println("Hello world!\n");
-        System.out.println(CollegeInterface.formattedString);
+        User user = new User();
+        user.dump();
     }
 }
